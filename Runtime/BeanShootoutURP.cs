0b1e19c1ebc1185439d3253000752fc7e7b3406f
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2026-02-02 20:11:47 -0500

    Disable EarlyCmd by default

--- Runtime/BeanShootoutURP.cs
+++ Runtime/BeanShootoutURP.cs
@@ -1,5 +1,21 @@
+using UnityEditor;
+
+[InitializeOnLoad]
 public static class BeanShootoutURP
 {
+    static BeanShootoutURP()
+    {
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+
+    private static void OnPlayModeStateChanged(PlayModeStateChange obj)
+    {
+        if (obj == PlayModeStateChange.ExitingPlayMode)
+        {
+            EnableEarlyCmd = false;
+        }
+    }
+
     /// <summary>
     /// Determines if XR rendering should be done.
     /// </summary>
@@ -8,6 +24,6 @@ public static class BeanShootoutURP
     /// <summary>
     /// Determines if EarlyCmd should be rendered. (EarlyCmd is the dark gray backgrond rendered behind everything to prevent nothing from being repainted)
     /// </summary>
-    public static bool EnableEarlyCmd = true;
+    public static bool EnableEarlyCmd;
 #endif
 }
